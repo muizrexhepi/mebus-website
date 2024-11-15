387850f37b5a463874141bae278929eeb847e6b6
etnik2002 <etnikz2002@gmail.com>
2024-11-15 22:13:00 +0100

    env

--- environment.ts
+++ environment.ts
@@ -1,6 +1,6 @@
 export const environment = { 
     // apiurl: "http://localhost:1234",
-    apiurl: "https://mebus-api.onrender.com",
+    apiurl: "https://api.gobusly.com",
     // domainurl: "http://localhost:3000",
     domainurl: "https://www.gobusly.com",
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: "pk_test_51K1DdaDAZApOs2EVXCiMmQnlAa9TIqCpnuhrDrpiKqdTGuGlNvbbyYnaEPgl2m0Qg2WfBC6r6j2wfP2jLdDwPdnm00D2bcqz6v",
