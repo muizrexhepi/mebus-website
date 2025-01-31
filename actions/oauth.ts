a62e96b9e27d8c9b92e2874c26dd34258f12f4d8
muizrexhepi <007lazi@gmail.com>
2025-01-31 09:34:20 +0100

    func: oauth

--- actions/oauth.ts
+++ actions/oauth.ts
@@ -3,21 +3,26 @@ import { OAuthProvider } from "appwrite";
 import axios from "axios";
 
 export async function handleGoogleLogin() {
+  if (/Mobi|Android/i.test(navigator.userAgent)) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
   account.createOAuth2Session(
     OAuthProvider.Google,
     // 'http://localhost:3000?oauth=true',
     // 'http://localhost:3000/fail',
     process.env.NEXT_PUBLIC_BASE_URL + "?oauth=true",
     `${process.env.NEXT_PUBLIC_BASE_URL}/fail`
-  )
+  );
 }
 
-
 export async function handleFacebookLogin() {
+  if (/Mobi|Android/i.test(navigator.userAgent)) {
+    await new Promise((resolve) => setTimeout(resolve, 100));
+  }
   account.createOAuth2Session(
     OAuthProvider.Facebook,
     // 'http://localhost:3000?oauth=true',
-    // 'http://localhost:3000/fail',
+    // 'http://localhost:3000/fail',q
     process.env.NEXT_PUBLIC_BASE_URL + "?oauth=true",
     `${process.env.NEXT_PUBLIC_BASE_URL}/fail`
   );
@@ -26,19 +31,21 @@ export async function handleFacebookLogin() {
 export async function handleOauthCallback() {
   try {
     const user = await account.get();
-    const newUser = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/user/create/db?oauth=true`, {
-      name: user.name,
-      email: user.email,
-      password: null,
-      appwrite_id: user.$id
-    })
+    const newUser = await axios.post(
+      `${process.env.NEXT_PUBLIC_API_URL}/user/create/db?oauth=true`,
+      {
+        name: user.name,
+        email: user.email,
+        password: null,
+        appwrite_id: user.$id,
+      }
+    );
 
     return newUser;
   } catch (error) {
     console.error(error);
   }
 }
-
 
 export interface IOauthCallbackUser {
   name: string;
