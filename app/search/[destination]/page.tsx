6656ecf3b8fac25cb4bf76c15ad3c68d609b9863
muizrexhepi <007lazi@gmail.com>
2025-08-21 17:40:17 +0200

    search page images added on opengraph

--- app/search/[destination]/page.tsx
+++ app/search/[destination]/page.tsx
@@ -73,6 +73,13 @@ export async function generateMetadata({
       siteName: "GoBusly",
       locale: "en_US",
       type: "website",
+      images: [
+        {
+          url: `${process.env.NEXT_PUBLIC_BASE_URL}/search/${destination}/opengraph-image`,
+          width: 1200,
+          height: 630,
+        },
+      ],
     },
     twitter: {
       card: "summary_large_image",
